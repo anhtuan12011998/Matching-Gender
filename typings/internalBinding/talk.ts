eaf2ffdf9a04458f9caeaace975d543609c56695
anhtuan12011998 <anhtuan140212@gmail.com>
2023-07-30 09:35:05 +0700

    add interview

--- typings/internalBinding/talk.ts
+++ typings/internalBinding/talk.ts
@@ -1,30 +1,18 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<p>Please input a number between 5 and 10:</p>
-
-<input id="demo" type="text">
-<button type="button" onclick="myFunction()">Test Input</button>
-<p id="p01"></p>
-
-<script>
 function myFunction() {
   const message = document.getElementById("p01");
   message.innerHTML = "";
   let x = document.getElementById("demo").value;
   try {
-    if(x.trim() == "") throw "empty";
-    if(isNaN(x)) throw "not a number";
+    if(x.trim() == "") throw "is empty";
+    if(isNaN(x)) throw "is not a number";
     x = Number(x);
-    if(x < 5) throw "too low";
-    if(x > 10) throw "too high";
+    if(x > 10) throw "is too high";
+    if(x < 5) throw "is too low";
   }
   catch(err) {
-    message.innerHTML = "Input is " + err;
+    message.innerHTML = "Error: " + err + ".";
+  }
+  finally {
+    document.getElementById("demo").value = "";
   }
 }
-</script>
-
-</body>
-</html>
