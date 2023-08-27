8d0fa42db05690a9ffe67aacd83269c2da6809a2
anhtuan12011998 <anhtuan140212@gmail.com>
2023-08-27 10:11:33 +0700

    add run

---
+++ src/quic/run.h
@@ -0,0 +1,11 @@
+const person = {
+  firstName:"John",
+  lastName: "Doe",
+  display: function () {
+    let x = document.getElementById("demo");
+    x.innerHTML = this.firstName + " " + this.lastName;
+  }
+}
+
+let display = person.display.bind(person);
+setTimeout(display, 3000);
